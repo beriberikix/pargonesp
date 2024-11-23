e5d69fbdca12088f5e8c8e626c2292b3afa92583
Jonathan Beri <jmberi@gmail.com>
2024-11-23 13:47:10 -0800

    Add back pip install
    Signed-off-by: Jonathan Beri <jmberi@gmail.com>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -3,7 +3,7 @@
 west init -l app
 west update --narrow -o=--depth=1
 west zephyr-export
-# pip install -r deps/zephyr/scripts/requirements.txt
+pip install -r deps/zephyr/scripts/requirements.txt
 west completion bash > $HOME/west-completion.bash
 echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
 history -c
